1ed0060aab6a8d29c0c87f6ebf39d73562983405
Nil <554157247@qq.com>
2018-08-01 09:11:49 +0800

    study

--- app.js
+++ app.js
@@ -43,4 +43,42 @@ app.use((err, req, res, next) => {
   res.render('error');
 });
 
+
+// {
+//   let co = require('co');
+//   // 登录请求
+//   let loginReq = new Promise((resolve, reject) => {
+//     setTimeout(function () {
+//       resolve({success: true});
+//     }, 2000);
+//   });
+//
+//   // 获取用户信息
+//   let userInfoReq = new Promise((resolve, reject) => {
+//     setTimeout(function () {
+//       resolve({nickName: 'dounine'});
+//     }, 2000);
+//   });
+//
+//   // 异步处理过程
+//   loginReq.then(res => {
+//     if (res.success) {
+//       userInfoReq.then(userInfo => {
+//         console.log('获取成功');
+//         // 如果还有信赖, 需要继续写, 还没有逻辑业务参与
+//       });
+//     }
+//   });
+//
+//   // 同步处理过程
+//   co(function* () {
+//     let loginInfo = yield loginReq;
+//     if (loginInfo.success) {
+//       let userInfo = yield userInfoReq;
+//       console.log('获取成功');
+//     }
+//   });
+// }
+
+
 module.exports = app;
